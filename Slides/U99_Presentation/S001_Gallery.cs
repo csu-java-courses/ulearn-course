7d3dc0a7b7f680ddf6986979d926072e55f0bf34
xaos <vladimir.p.polyakov@gmail.com>
2018-09-06 10:40:24 +0500

    Initial commit

---
+++ Slides/U99_Presentation/S001_Gallery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uLearn;
+
+namespace Presentation.Slides.U99_Presentation
+{
+	[Slide("Галерея", "00daeeb0-4c61-4521-9b8c-ba4df2911b58")]
+	class S001_Gallery
+	{
+		//#gallery img
+	}
+}
